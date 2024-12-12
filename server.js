1a514e21ffbf126e4adbd288eb82ab60ed891f05
ItsMeRanajit <ranajit104das@gmail.com>
2024-12-12 22:34:28 +0530

    fixes

--- server.js
+++ server.js
@@ -3,20 +3,14 @@ import { Server } from "socket.io";
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
-  cors: {
-    origin: "https://realtime-tic-tac-toe.vercel.app",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type"],
-    credentials: true,
-  },
-  transports: ["websocket"],
+  cors: "http://localhost:5173/",
 });
 
 const allUsers = new Map();
-let roomName;
 
 io.on("connection", (socket) => {
   let opponent = null;
+  socket.roomName = null;
   allUsers.set(socket.id, { Socket: socket, online: true, playing: false, username: "demo", playingAs: "" });
 
   socket.on("req_to_play", (data) => {
@@ -28,21 +22,23 @@ io.on("connection", (socket) => {
     for (let [id, user] of allUsers) {
       if (user.online && !user.playing && id !== socket.id && user.username !== "demo") {
         currUser.playing = true;
+        currUser.Socket.roomName = `room-${currUser.username}-${currUser.Socket.id}`;
         allUsers.set(socket.id, currUser);
         opponent = user;
         opponent.playing = true;
+        opponent.Socket.roomName = currUser.Socket.roomName;
         allUsers.set(opponent.Socket.id, opponent);
         break;
       }
     }
 
     if (opponent) {
-      roomName = `room-${currUser.username}-${currUser.Socket.id}`;
-      socket.join(roomName);
-      opponent.Socket.join(roomName);
-
-      io.to(roomName).emit("roomJoined", {
-        room: roomName,
+      // console.log(opponent.Socket.roomName, currUser.Socket.roomName);
+      // console.log(opponent, currUser);
+      socket.join(currUser.Socket.roomName);
+      opponent.Socket.join(opponent.Socket.roomName);
+      io.to(socket.roomName).emit("roomJoined", {
+        room: socket.roomName,
         players: {
           currentPlayer: { playerId: currUser.Socket.id, playerName: currUser.username, playingAs: "cross" },
           opponentPlayer: { playerId: opponent.Socket.id, playerName: opponent.username, playingAs: "circle" },
@@ -54,25 +50,31 @@ io.on("connection", (socket) => {
   });
 
   socket.on("messageSent", function (data) {
-    console.log(data);
-    io.to(roomName).emit("messageRecieved", {
+    // console.log(data);
+    io.to(socket.roomName).emit("messageRecieved", {
       message: data,
       sender: socket.id,
     });
   });
 
   socket.on("playerMoveUser", function (data) {
-    console.log(data);
-    io.to(roomName).emit("updateState", { ...data });
+    // console.log(data);
+    if (socket.roomName) {
+      io.to(socket.roomName).emit("updateState", { ...data });
+    }
   });
 
   socket.on("disconnect", function () {
-    io.to(roomName).emit("playerExit");
-    allUsers.delete(socket.id);
+    if (socket.roomName) {
+      io.to(socket.roomName).emit("playerExit");
+      allUsers.delete(socket.id);
+    }
   });
   socket.on("removePlayer", function () {
-    allUsers.delete(socket.id);
-    io.to(roomName).emit("playerExit");
+    if (socket.roomName) {
+      allUsers.delete(socket.id);
+      io.to(socket.roomName).emit("playerExit");
+    }
   });
   socket.on("removePlayerFromSet", function () {
     allUsers.delete(socket.id);
